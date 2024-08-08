f607e1ad96caa8b2ff0eb8c313111b360b4cd92e
abenomran <abdalla.benomran2@gmail.com>
2024-08-08 13:44:29 -0400

    updated system prompt and chat styling

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,44 +3,68 @@ const userInput = document.querySelector("#userInput");
 const userInputForm = document.querySelector("#user-input-form");
 const sendBtn = document.querySelector("#send-button");
 
+async function sendTest() {
+  const response = await fetch("http://127.0.0.1:5000/api/test", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+  });
+
+  let resData = await response.json();
+}
+
 async function sendMessage() {
   const userMessage = userInput.value;
 
-  chatLog.innerHTML += `<p>${userMessage}</p>`;
+  chatLog.innerHTML += `<p class="user-text-bubble rounded-pill px-2 py-1 text-white ml-auto">${userMessage}</p>`;
   userInput.value = "";
   scrollToBottom();
 
-  const response = await fetch("/chat", {
+  const response = await fetch("http://127.0.0.1:5000/api/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ message: userMessage }),
+    mode: "cors",
   });
 
-  chatLog.innerHTML += `<p><strong>Bot: </strong></p>`;
+  chatLog.innerHTML += `<p class="px-2 pt-1 mr-auto"></p>`;
   let lastParagraph = chatLog.querySelector("p:last-child");
 
   const reader = response.body.getReader();
+  const decoder = new TextDecoder();
   let output = "";
 
   while (true) {
     const { done, value } = await reader.read();
-    decodedValue = new TextDecoder().decode(value);
-    if (decodedValue === "\n") {
-      output += "<br></br>";
-    } else {
-      output += decodedValue;
-    }
-
-    console.log(output);
-    //lastParagraph.innerHTML += output;
-    lastParagraph.innerHTML = `<p><strong>Bot: </strong>${output}</p>`;
-    scrollToBottom();
 
     if (done) {
       return;
     }
+
+    if (value) {
+      const decodedValue = decoder.decode(value, { stream: true });
+      output += decodedValue.replace(/\n/g, "<br>");
+    } else {
+      continue;
+    }
+
+    // Dynamically switch class based on the content length
+    if (output.length > 200) {
+      // Adjust this threshold as needed
+      lastParagraph.classList.remove("rounded-pill");
+      lastParagraph.classList.add("rounded-lg");
+    } else {
+      lastParagraph.classList.add("rounded-pill");
+      lastParagraph.classList.remove("rounded-lg");
+    }
+
+    //lastParagraph.innerHTML += output;
+    lastParagraph.innerHTML = `<p class="px-2 pt-1 mr-auto">${output}</p>`;
+    scrollToBottom();
   }
 }
 
@@ -61,4 +85,6 @@ userInputForm.addEventListener("input", function () {
 userInputForm.addEventListener("submit", function (e) {
   e.preventDefault();
   sendMessage();
+
+  //sendTest();
 });
